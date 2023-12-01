2bb28796f6b51d143c7c629aa77c55ef890cf8a0
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-12-01 18:51:06 +0530

    Initial commit

---
+++ components/Editor.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
+
+import { useEdgeStore } from "@/lib/edgestore";
+
+const Editor = ({ onChange, initialContent, editable }) => {
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file) => {
+    const response = await edgestore.publicFiles.upload({
+      file,
+    });
+
+    return response.url;
+  };
+
+  const editor = useBlockNote({
+    editable,
+    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
+    onEditorContentChange: (editor) => {
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+    },
+    uploadFile: handleUpload,
+  });
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+      />
+    </div>
+  );
+};
+
+export default Editor;
