98a68ba1b11b609081d46a6a99cf60cde6111685
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-12-03 23:21:03 +0530

    Initial commit

---
+++ components/modals/CoverImageModal.jsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState } from "react";
+import { useMutation } from "convex/react";
+import { useParams } from "next/navigation";
+
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
+import { useCoverImage } from "@/hooks/use-cover-image";
+import { useEdgeStore } from "@/lib/edgestore";
+import { api } from "@/convex/_generated/api";
+import SingleImageDropzone from "@/components/SingleImageDropZone";
+
+const CoverImageModal = () => {
+  const params = useParams();
+  const update = useMutation(api.documents.update);
+  const coverImage = useCoverImage();
+  const { edgestore } = useEdgeStore();
+
+  const [file, setFile] = useState();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onClose = () => {
+    setFile(undefined);
+    setIsSubmitting(false);
+    coverImage.onClose();
+  };
+
+  const onChange = async (file) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
+
+      const res = await edgestore.publicFiles.upload({
+        file,
+        options: {
+          replaceTargetUrl: coverImage.url,
+        },
+      });
+
+      await update({
+        id: params.documentId,
+        coverImage: res.url,
+      });
+
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <h2 className="text-center text-lg font-semibold">Cover Image</h2>
+        </DialogHeader>
+        <SingleImageDropzone
+          className="w-full outline-none"
+          disabled={isSubmitting}
+          value={file}
+          onChange={onChange}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CoverImageModal;
