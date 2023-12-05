1604da00cdcf67aea156226340a4da3e3b47de7c
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-12-05 22:19:43 +0530

    Initial commit

---
+++ app/(main)/layout.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Spinner from "@/components/Spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import React from "react";
+import Navigation from "./_components/Navigation";
+import SearchCommand from "@/components/SearchCommand";
+
+const MainLayout = ({ children }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">
+        <SearchCommand />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default MainLayout;
