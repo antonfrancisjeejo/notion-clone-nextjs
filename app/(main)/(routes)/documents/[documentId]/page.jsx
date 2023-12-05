8c2685bb97f588726ce17d8048d6dea5c0a00459
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-12-05 22:19:29 +0530

    Initial commit

---
+++ app/(main)/(routes)/documents/[documentId]/page.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useMutation, useQuery } from "convex/react";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
+import { api } from "@/convex/_generated/api";
+import { Skeleton } from "@/components/ui/skeleton";
+import Toolbar from "@/components/Toolbar";
+import Cover from "@/components/Cover";
+import Editor from "@/components/Editor";
+
+const DocumentIdPage = ({ params }) => {
+  // const Editor = useMemo(
+  //   () => dynamic(() => import("@/components/Editor"), { ssr: false }),
+  //   []
+  // );
+
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content) => {
+    update({
+      id: params.documentId,
+      content,
+    });
+  };
+
+  if (document === undefined) {
+    return (
+      <div>
+        <Cover.Skeleton />
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <Cover url={document.coverImage} />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+        <Editor onChange={onChange} initialContent={document.content} />
+      </div>
+    </div>
+  );
+};
+
+export default DocumentIdPage;
